39fde4709a451c09f05cac93f299c0741a6335b7
zacharyrote <zachary.rote@gmail.com>
2017-02-21 08:32:48 -0500

    add descriptor side load

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
 const Server = require('core-server').Server;
+const Promise = require('promise');
 
-// Public
-module.exports.startTestService = (name, callback) => {
+
+const startTestService = (name, callback) => {
     let server = null;
     server = new Server(name, null, {types:[]}, {});
 
@@ -20,4 +21,38 @@ module.exports.startTestService = (name, callback) => {
         console.log(err);
         callback(err, null);
     });
-};
+}
+
+const sideLoadServiceDescriptor = (service, descriptor) => {
+    let p = new Promise((resolve, reject) => {
+        descriptor.id = uuid.v1();
+        if(service.boundProxy) {
+            service.getApp().proxy = service.boundProxy;
+
+            service.boundProxy.sideLoadService(descriptor).then(() => {
+                return service.boundProxy.table();
+            }).then((cache) => {
+                console.log(cache);
+                resolve(); 
+            }).catch((err) => {
+                reject(err);
+            });
+        } else {
+            service.boundProxy = new Proxy();
+            service.getApp().proxy = service.boundProxy;
+            service.boundProxy.sideLoadService(descriptor).then(() => {
+                return service.boundProxy.table();
+            }).then((cache) => {
+                console.log(cache);
+                resolve(); 
+            }).catch((err) => {
+                reject(err);
+            });
+        }
+    });
+    return p;
+}
+
+// Public
+module.exports.startTestService = startTestService;
+module.exports.sideLoadServiceDescriptor = sideLoadServiceDescriptor;
